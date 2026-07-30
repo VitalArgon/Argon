73bbc1f1e3311226888744155424998e920ab0bb
Zarak199076 <131101105+Zarak199076@users.noreply.github.com>
2026-07-29 22:31:46 -0500

    Add support for Veil plugins in PluginCard

--- src/components/settings/tabs/plugins/PluginCard.tsx
+++ src/components/settings/tabs/plugins/PluginCard.tsx
@@ -34,6 +34,7 @@ export function PluginCard({ plugin, disabled, onRestartNeeded, onMouseEnter, on
     const pluginMeta = PluginMeta[plugin.name];
     const isEquicordPlugin = pluginMeta.folderName.startsWith("src/equicordplugins/") ?? false;
     const isVencordPlugin = pluginMeta.folderName.startsWith("src/plugins/") ?? false;
+    const isVeilPlugin = pluginMeta.folderName.startsWith("src/veilplugins/") ?? false;
     const isUserPlugin = pluginMeta?.userPlugin ?? false;
     const isModifiedPlugin = plugin.isModified ?? false;
 
@@ -107,6 +108,12 @@ export function PluginCard({ plugin, disabled, onRestartNeeded, onMouseEnter, on
             src: "https://equicord.org/assets/icons/vencord/icon-light.png",
             alt: "Vencord",
             title: "Vencord Plugin"
+        },
+        {
+            condition: isVeilPlugin,
+            src: "https://raw.githubusercontent.com/Zarak199076/Veil/main/browser/icon.png",
+            alt: "Veil",
+            title: "Veil Plugin"
         },
         {
             condition: isUserPlugin,
