e062c1eac8e37f5926560b270449c691c824de75
Zarak_ <131101105+Zarak199076@users.noreply.github.com>
2026-08-02 23:04:03 -0500

    Add ChannelPopups to GuildPlugins export

--- src/veilcore/guildplugins/index.ts
+++ src/veilcore/guildplugins/index.ts
@@ -1,7 +1,9 @@
 import GuildTheme from "./guildTheme";
 import CustomChannelNames from "./customChannelNames";
+import ChannelPopups from "./channelPopups";
 
 export const GuildPlugins: Record<string, ReturnType<typeof import("./_api/defineGuildPlugin").defineGuildPlugin>> = {
     guildTheme: GuildTheme,
     customChannelNames: CustomChannelNames,
+    channelPopups: ChannelPopups,
 };
