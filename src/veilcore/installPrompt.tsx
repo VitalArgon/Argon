e2efefcd10fb535295e0247b6878159fdaa948de
Zarak_ <131101105+Zarak199076@users.noreply.github.com>
2026-08-02 21:17:06 -0500

    Refactor installPrompt to use DataStore for persistence

--- src/veilcore/installPrompt.tsx
+++ src/veilcore/installPrompt.tsx
@@ -1,30 +1,22 @@
-import { ModalRoot, ModalHeader, ModalContent, ModalFooter, openModal } from "@utils/modal";
-import { Button, Text } from "@webpack/common";
-import { GuildManifestEntry } from "@veilcore/guildplugins/manifest";
-import { setUserOptIn } from "./guildPluginManager";
-
+import * as DataStore from "@api/DataStore";
+// ...
 const askedKey = (guildId: string, pluginId: string) => `Veil_guildPlugin_asked_${guildId}_${pluginId}`;
 
-export function maybeShowInstallPrompt(entry: GuildManifestEntry, pluginId: string) {
+export async function maybeShowInstallPrompt(entry: GuildManifestEntry, pluginId: string) {
     const key = askedKey(entry.guildId, pluginId);
-    if (localStorage.getItem(key) === "true") return;
+    if (await DataStore.get(key) === true) return;
 
     openModal(props => (
         <ModalRoot {...props}>
-            <ModalHeader>
-                <Text variant="heading-lg/semibold">{entry.guildName} has a plugin for you</Text>
-            </ModalHeader>
-            <ModalContent>
-                <Text>This server offers the "{pluginId}" plugin. Enable it for this server only?</Text>
-            </ModalContent>
+            {/* ... */}
             <ModalFooter>
-                <Button onClick={() => {
-                    localStorage.setItem(key, "true");
-                    setUserOptIn(entry.guildId, pluginId, true);
+                <Button onClick={async () => {
+                    await DataStore.set(key, true);
+                    await setUserOptIn(entry.guildId, pluginId, true);
                     props.onClose();
                 }}>Enable</Button>
-                <Button color={Button.Colors.TRANSPARENT} onClick={() => {
-                    localStorage.setItem(key, "true");
+                <Button color={Button.Colors.TRANSPARENT} onClick={async () => {
+                    await DataStore.set(key, true);
                     props.onClose();
                 }}>No thanks</Button>
             </ModalFooter>
