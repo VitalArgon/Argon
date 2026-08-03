b7c23b0d85edd7b27eec06bde4061c68473c17de
Zarak_ <131101105+Zarak199076@users.noreply.github.com>
2026-08-02 21:39:19 -0500

    Add stopGuildPluginManager function to deactivate plugins

--- src/veilcore/guildPluginManager.ts
+++ src/veilcore/guildPluginManager.ts
@@ -64,3 +64,11 @@ export function initGuildPluginManager() {
         handleGuildAvailable((guild as any).id);
     }
 }
+
+let subscriptions: (() => void)[] = [];
+
+export function stopGuildPluginManager() {
+    for (const pluginId of Object.keys(GuildPlugins)) {
+        deactivate(pluginId);
+    }
+}
