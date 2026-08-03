97990153c5176805cdb16faae447b4b21a96a6cd
Zarak_ <131101105+Zarak199076@users.noreply.github.com>
2026-08-02 19:09:35 -0500

    Delete src/veilplugins/guildplugins directory

--- src/veilplugins/guildplugins/_api/defineGuildPlugin.ts
+++
@@ -1,5 +0,0 @@
-import definePlugin from "@utils/types";
-
-export function defineGuildPlugin(def: Parameters<typeof definePlugin>[0]) {
-    return definePlugin(def);
-}
--- src/veilplugins/guildplugins/index.ts
+++
@@ -1,7 +0,0 @@
-import WelcomeBanner from "./welcomeBanner";
-import CustomChannelNames from "./customChannelNames";
-
-export const GuildPlugins: Record<string, ReturnType<typeof import("./_api/defineGuildPlugin").defineGuildPlugin>> = {
-    welcomeBanner: WelcomeBanner,
-    customChannelNames: CustomChannelNames,
-};
