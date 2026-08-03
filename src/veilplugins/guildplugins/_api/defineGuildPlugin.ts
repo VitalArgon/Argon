29be2e33decb74b7e07a8cc32dfb1b3d48ee6625
Zarak_ <131101105+Zarak199076@users.noreply.github.com>
2026-08-02 19:02:08 -0500

    Refactor defineGuildPlugin parameter type

--- src/veilplugins/guildplugins/_api/defineGuildPlugin.ts
+++ src/veilplugins/guildplugins/_api/defineGuildPlugin.ts
@@ -1,5 +1,5 @@
-import definePlugin, { PluginDef } from "@utils/types";
+import definePlugin from "@utils/types";
 
-export function defineGuildPlugin(def: PluginDef) {
+export function defineGuildPlugin(def: Parameters<typeof definePlugin>[0]) {
     return definePlugin(def);
 }
