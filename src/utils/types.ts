1df14ba68c891195a9d8088cfad3d2b06615eff6
Zarak_ <131101105+Zarak199076@users.noreply.github.com>
2026-08-04 12:16:15 -0500

    Add isVeilModified to PluginDef

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ */
 
 import { AudioProcessor } from "@api/AudioPlayer";
 import type { ProfileBadge } from "@api/Badges";
@@ -246,6 +246,11 @@ export interface PluginDef {
      * A Vencord plugin that is modified for extra features in Equicord
      */
     isModified?: boolean;
+
+    /**
+     * An Equicord plugin that has been further modified by Veil
+     */
+    isVeilModified?: boolean;
 }
 
 export const enum StartAt {
