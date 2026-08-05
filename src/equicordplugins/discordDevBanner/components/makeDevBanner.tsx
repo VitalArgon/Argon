b6a1a16b0641c098cf03180c5db85e5404f4b9fc
Zarak_ <131101105+Zarak199076@users.noreply.github.com>
2026-08-05 14:51:51 -0500

    Replace EquicordIcon with veilIcon in makeDevBanner

--- src/equicordplugins/discordDevBanner/components/makeDevBanner.tsx
+++ src/equicordplugins/discordDevBanner/components/makeDevBanner.tsx
@@ -10,13 +10,13 @@ import { gitHashShort } from "@shared/vencordUserAgent";
 import { React } from "@webpack/common";
 import { JSX } from "react";
 
-import { ChromiumIcon, ClientIcon, DevBannerIcon, DiscordIcon, ElectronIcon, EquicordIcon, names, settings } from ".";
+import { ChromiumIcon, ClientIcon, DevBannerIcon, DiscordIcon, ElectronIcon, veilIcon, names, settings } from ".";
 
 export function makeDevBanner(state?: string): string | JSX.Element {
     const { RELEASE_CHANNEL, BUILD_NUMBER, VERSION_HASH } = window.GLOBAL_ENV;
     const buildChannel = names[RELEASE_CHANNEL] || RELEASE_CHANNEL.charAt(0).toUpperCase() + RELEASE_CHANNEL.slice(1);
     const { chromiumVersion, electronVersion, getVersionInfo } = SettingsPlugin;
-    const format = settings.store.format ?? "{devbannerIcon} {buildChannel} {buildNumber} ({buildHash}) | {equicordIcon} {equicordName} {equicordVersion} ({equicordHash})";
+    const format = settings.store.format ?? "{devbannerIcon} {buildChannel} {buildNumber} ({buildHash}) | {veilIcon} {equicordName} {equicordVersion} ({equicordHash})";
     const baseFormat = state ?? format;
 
     const clientInfo = detectClient();
