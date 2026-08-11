091ded127076b4aa5b3749e3f15d29b5ba393259
Zarak_ <131101105+Zarak199076@users.noreply.github.com>
2026-08-10 22:07:46 -0500

    Update index.ts

--- src/argonplugins/guildPluginsFramework/index.ts
+++ src/argonplugins/guildPluginsFramework/index.ts
@@ -1,12 +1,12 @@
 import definePlugin from "@utils/types";
-import { VeilDevs } from "@utils/constants";
-import { initGuildPluginManager, stopGuildPluginManager } from "@veilcore/guildPluginManager";
-import { registerGuildPluginsTab } from "@veilcore/registerGuildPluginsTab";
+import { ArgonDevs } from "@utils/constants";
+import { initGuildPluginManager, stopGuildPluginManager } from "@argoncore/guildPluginManager";
+import { registerGuildPluginsTab } from "@argoncore/registerGuildPluginsTab";
 
 export default definePlugin({
     name: "GuildPluginsFramework",
     description: "Activates guild-gated plugins for guilds you're in, and adds the Guild Plugins settings tab.",
-    authors: [VeilDevs.Zarak],
+    authors: [ArgonDevs.Zarak],
     required: true,
 
     start() {
